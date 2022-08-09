7dba78ee4514fa4bc121f82a19339f9e950c8f5c
Remix Workspace <>
2022-08-09 23:15:51 +0530

    constants, defaultvalues examples are added

---
+++ contracts/Constants-11.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // 21420 gas
+    uint public constant MY_UNIT = 222;
+}
+
+contract Var {
+    address public  MY_ADDRESS = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // 23553 gas
+}
