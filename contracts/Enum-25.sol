0f85b906ee8c8ba06b627e12a55bdcb5f6f6bc14
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-08-26 00:24:25 +0530

    Enum example added

---
+++ contracts/Enum-25.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+contract Enum {
+    enum Status {
+        None,
+        Pending,
+        Shipped,
+        Completed,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+
+    struct Order {
+        address buyer;
+        Status status;
+    }
+
+    Order[] public orders;
+
+    function get() external view returns(Status) {
+        return status; 
+    }
+
+    function set(Status _status) external {
+        status = _status;
+    }
+
+    function ship() external {
+        status = Status.Shipped;
+    }
+
+    function reset() external {
+        delete status;
+    }
+}
