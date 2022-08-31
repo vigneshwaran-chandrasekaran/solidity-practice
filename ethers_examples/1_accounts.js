39c3bd7a4ee2317ecfa3550ef2aa79c796d43ca7
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-09-01 00:32:23 +0530

    to get the balance of particular address done

--- ethers_examples/1_accounts.js
+++ ethers_examples/1_accounts.js
@@ -1,6 +1,22 @@
 const { ethers } = require("ethers");
 require("dotenv").config();
 
-console.log(process.env.INFURA_API_KEY);
+const INFURA_ID = process.env.INFURA_API_KEY;
 
-// ethers.providers.JsonRpcBatchProvider("");
+const address = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
+const address_one = "0x679688540a567be87625bEEf0BBAB7e37F704A46";
+
+const provider = new ethers.providers.JsonRpcBatchProvider(
+  `https://mainnet.infura.io/v3/${INFURA_ID}`
+);
+
+const main = async (adrs) => {
+  const balance = await provider.getBalance(adrs);
+
+  console.log(
+    `\nETH Balance of ${adrs} --> ${ethers.utils.formatEther(balance)} ETH\n`
+  );
+};
+
+main(address);
+main(address_one);
